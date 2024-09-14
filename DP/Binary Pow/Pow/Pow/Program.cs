33bc61a9e9c757341fddd0be19ba9dd850fcd469
Artem Chuvaiev <137652420+tmprokill@users.noreply.github.com>
2024-09-14 19:35:38 +0300

    binary exponentiation

---
+++ DP/Binary Pow/Pow/Pow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+namespace Pow;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Pow(2, 15));
+    }
+
+    static int Pow (int a, int n)
+    {
+        int result = 1;
+        while (n > 0)
+        {
+            if (n % 2 == 1)
+            {
+                result *= a;
+            }
+
+            a *= a;
+            n /= 2;
+        }
+
+        return result;
+    }
+}
